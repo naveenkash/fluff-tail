e95488d165e6e48bb53582d5618c55cca654fdba
Naveen Kashyap <naveenkashyap243@gmail.com>
2020-06-27 02:09:52 +0530

    🎡add prop type checking and add default props

--- components/card/RainbowStackedAccordian/index.js
+++ components/card/RainbowStackedAccordian/index.js
@@ -1,15 +1,51 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./style.scss";
 export default class RainbowStackedAccordian extends Component {
-  truncate = () => {
-    console.log(this.props.truncate);
-  }
+  static defaultProps = {
+    headingColor: "#000000",
+    descColor: "#000000",
+    disabled: false,
+    openNewPage: false,
+  };
+
   render() {
     return (
       <div className="rainbow-card">
-        <p>{this.props.title}</p>
-        <h2>{this.props.desc}</h2>
+        <a
+          href={this.props.disabled ? "javascript: void (0)" : this.props.href}
+          target={this.props.openNewPage ? "_blank" : "_self"}
+        >
+          {(() => {
+            if (this.props.title) {
+              return (
+                <p style={{ color: this.props.headingColor }}>
+                  {this.props.title}
+                </p>
+              );
+            }
+          })()}
+          {(() => {
+            if (this.props.title) {
+              return (
+                <h2 style={{ color: this.props.descColor }}>
+                  {this.props.desc}
+                </h2>
+              );
+            }
+          })()}
+        </a>
       </div>
     );
   }
 }
+
+RainbowStackedAccordian.propTypes = {
+  title: PropTypes.string,
+  desc: PropTypes.string,
+  href: PropTypes.string,
+  descColor: PropTypes.string,
+  headingColor: PropTypes.string,
+  disabled: PropTypes.bool,
+  openNewPage: PropTypes.bool,
+};
