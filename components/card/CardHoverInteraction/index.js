8aebad371e33de5ddda1e5287090399dac0bab72
Naveen Kashyap <naveenkashyap243@gmail.com>
2020-06-27 02:08:46 +0530

    🎢 add prop type checing and add default props

--- components/card/CardHoverInteraction/index.js
+++ components/card/CardHoverInteraction/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./style.scss";
 function CardHoverInteraction(props) {
   return (
@@ -6,10 +7,32 @@ function CardHoverInteraction(props) {
       <div className="content">
         <h2 className="title">{props.title}</h2>
         <p className="copy">{props.desc}</p>
-        <button className="btn">{props.btnTitle}</button>
+        {(() => {
+          if (props.showBtn) {
+            return (
+              <a
+                className="btn"
+                href={props.href ? props.href : "javascript: void (0)"}
+                target={props.openNewPage ? "_blank" : "_self"}
+                tabIndex="0"
+              >
+                {props.btnTitle}
+              </a>
+            );
+          }
+        })()}
       </div>
     </div>
   );
 }
-
+CardHoverInteraction.defaultProps = {
+  showBtn: true,
+};
+CardHoverInteraction.propTypes = {
+  href: PropTypes.string,
+  showBtn: PropTypes.bool,
+  desc: PropTypes.string,
+  title: PropTypes.string,
+  openNewPage: PropTypes.bool,
+};
 export default CardHoverInteraction;
