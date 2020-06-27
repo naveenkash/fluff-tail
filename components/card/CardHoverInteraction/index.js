c5a3eceec030f1c33f13d33e1c8f291637f286f7
Naveen Kashyap <naveenkashyap243@gmail.com>
2020-06-27 15:37:48 +0530

    add 🚀 dynamic background image

--- components/card/CardHoverInteraction/index.js
+++ components/card/CardHoverInteraction/index.js
@@ -4,6 +4,7 @@ import "./style.scss";
 function CardHoverInteraction(props) {
   return (
     <div className="interaction-card">
+      <img src={props.image} alt={props.alt} />
       <div className="content">
         <h2 className="title">{props.title}</h2>
         <p className="copy">{props.desc}</p>
@@ -12,7 +13,7 @@ function CardHoverInteraction(props) {
             return (
               <a
                 className="btn"
-                href={props.href ? props.href : "javascript: void (0)"}
+                href={props.href ? props.href : "/#"}
                 target={props.openNewPage ? "_blank" : "_self"}
                 tabIndex="0"
               >
@@ -25,10 +26,17 @@ function CardHoverInteraction(props) {
     </div>
   );
 }
+
 CardHoverInteraction.defaultProps = {
   showBtn: true,
+  alt: "Image",
+  image:
+    "https://images.unsplash.com/photo-1517021897933-0e0319cfbc28?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
 };
+
 CardHoverInteraction.propTypes = {
+  image: PropTypes.string,
+  alt: PropTypes.string,
   href: PropTypes.string,
   showBtn: PropTypes.bool,
   desc: PropTypes.string,
