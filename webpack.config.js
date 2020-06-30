9c00f19eaed9161432936f32b3f5e9233a459c9f
Naveen Kashyap <naveenkashyap243@gmail.com>
2020-07-01 02:43:18 +0530

    add externals in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -29,5 +29,19 @@ module.exports = {
       },
     ],
   },
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+    },
+  },
   optimization: { minimize: true, minimizer: [new TerserPlugin()] },
 };
